f9bfa47bdb802351eb8d1d26ead5f239bcb3e73c
natnaree <mac@MacBook-Pro-khxng-natnaree.local>
2020-11-10 15:44:01 +0700

    howto

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,8 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Home(),
-    );
-  }
-
-}
-
-class Home extends StatefulWidget {
-  const Home();
-  @override
-  _HomeState createState() => new _HomeState();
-}
 
 class _HomeState extends State<Home> {
-
-  static const String flutterUrl = 'https://flutter.io/';
-  static const String githubUrl = 'http://www.codesnippettalk.com';
-
-  static const TextStyle linkStyle = const TextStyle(
-    color: Colors.blue,
-    decoration: TextDecoration.underline,
-  );
-
-  TapGestureRecognizer _flutterTapRecognizer;
-  TapGestureRecognizer _githubTapRecognizer;
 
   @override
   void initState() {
@@ -74,98 +44,6 @@ class _HomeState extends State<Home> {
           );
           // Perform some action
         },
-      ),
-    );
-  }
-
-  Widget _buildAboutDialog(BuildContext context) {
-    return new AlertDialog(
-      title: const Text('ข้อตกลงการสมัครสมาชิก Winwin'),
-      content: new Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          _buildAboutText(),
-          _buildLogoAttribution(),
-        ],
-      ),
-      actions: <Widget>[
-        new FlatButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          textColor: Theme.of(context).primaryColor,
-          child: const Text('ตกลง'),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildAboutText() {
-    return new RichText(
-      text: new TextSpan(
-        text: 'โดยการลงทะเบียนสำหรับบริการแอพ Winwin หรือการบริการใดๆของแอพพลิเคชั่น Winwin ท่านยินยอมที่จะผูกพันตามข้อตกลงและเงื่อนไข(“เงื่อนไขการให้บริการ”)บริการที่นำเสนอโดยแอพพลิเคชั่น Winwin อยู่ภายใต้เงื่อนไขการให้บริการ เพื่อช่วยให้ท่านมีการเดินทางที่สะดวก รวดเร็ว และราคาที่เท่าเทียม\n\n',
-        style: const TextStyle(color: Colors.black87),
-        // children: <TextSpan>[
-        //   const TextSpan(text: 'The app was developed with '),
-        //   new TextSpan(
-        //     text: 'Flutter',
-        //     recognizer: _flutterTapRecognizer,
-        //     style: linkStyle,
-        //   ),
-        //   const TextSpan(
-        //     text: ' and it\'s open source; check out the source '
-        //         'code yourself from ',
-        //   ),
-        //   new TextSpan(
-        //     text: 'www.codesnippettalk.com',
-        //     recognizer: _githubTapRecognizer,
-        //     style: linkStyle,
-        //   ),
-        //   const TextSpan(text: '.'),
-        //],
-      ),
-    );
-  }
-
-  Widget _buildLogoAttribution() {
-    return new Padding(
-      padding: const EdgeInsets.only(top: 16.0),
-      child: new Row(
-        children: <Widget>[
-          new Padding(
-            padding: const EdgeInsets.only(top: 0.0),
-            // child: new Image.asset(
-            //   "assets/flutter.png",
-            //   width: 32.0,
-            // ),
-          ),
-          // const Expanded(
-          //   child: const Padding(
-          //     padding: const EdgeInsets.only(left: 12.0),
-          //     child: const Text(
-          //       'Popup window is like a dialog box that gains complete focus when it appears on screen.',
-          //       style: const TextStyle(fontSize: 12.0),
-          //     ),
-          //   ),
-          // ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-
-      padding: const EdgeInsets.only(top: 100.0),
-
-      color: Theme.of(context).scaffoldBackgroundColor,
-
-      child: new Column(
-        children: <Widget>[
-          home(context),
-        ],
       ),
     );
   }
