205b7b79e9522c645c97e95e40915f1bac792463
natnaree <mac@MacBook-Pro-khxng-natnaree.local>
2020-11-10 15:44:18 +0700

    howto

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,26 @@
+import 'package:MyApp/terms_service.dart/help.dart';
 import 'package:flutter/material.dart';
-void main() => runApp(new MyApp());
 
+import './terms_service/help.dart';
 
-class _HomeState extends State<Home> {
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
   @override
-  void initState() {
-    super.initState();
-    _flutterTapRecognizer = new TapGestureRecognizer()..onTap = () => _openUrl(flutterUrl);
-    _githubTapRecognizer = new TapGestureRecognizer()..onTap = () => _openUrl(githubUrl);
-  }
-
-  @override
-  void dispose() {
-    _flutterTapRecognizer.dispose();
-    _githubTapRecognizer.dispose();
-    super.dispose();
-  }
-
-  void _openUrl(String url) async {
-    // Close the about dialog.
-    Navigator.pop(context);
-
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  Widget home(BuildContext context) {
-    return new Material(
-      child: new RaisedButton(
-        child: const Text('Show Pop-up'),
-        color: Theme.of(context).accentColor,
-        elevation: 4.0,
-        splashColor: Colors.amberAccent,
-        textColor: const Color(0xFFFFFFFF),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) => _buildAboutDialog(context),
-          );
-          // Perform some action
-        },
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false ,
+      theme: ThemeData(
+        primarySwatch:Colors.blue,
       ),
+      home: Help(),
+      
     );
   }
 }
+
+
+
